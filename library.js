ccb0113e34c365e34998c09f1f383fc22c74260e
rloftus215 <rloftus215@gmail.com>
2024-06-22 14:28:05 +0100

    Add fucntion for add Book button

--- library.js
+++ library.js
@@ -1,5 +1,56 @@
+const myLibrary = []
+ 
 
+ 
+ const titleInput = document.querySelector('#title');
+ const authorInput = document.querySelector('#author');
+ const pagesInput = document.querySelector('#pages');
+ const table = document.querySelector('table');
+ 
 
+ 
+ function addNewBook() {
+ 
+ 
+ let newObject = {
+ }
+ 
+ 
+ newObject.title = `${titleInput.value}`;
+ newObject.author = `${authorInput.value}`;
+ newObject.pages = `${pagesInput.value}`;
+ 
+ 
+ myLibrary.push(newObject)
+
+ const tr = document.createElement('tr');
+ const title = document.createElement('td');
+ const author = document.createElement('td');
+ const pages = document.createElement('td');
+ const deleteButton = document.createElement('button');
+ title.innerText = `${newObject.title}`;
+ author.innerText = `${newObject.author}`;
+ pages.innerText = `${newObject.pages}`;
+ deleteButton.innerText = 'delete';
+ tr.appendChild(title);
+ tr.appendChild(author);
+ tr.appendChild(pages);
+ tr.appendChild(deleteButton);
+ table.appendChild(tr);
+    
+}
+ 
+ 
+ 
+ document.addEventListener('keydown', function (e) {
+    if(e.key === 'Enter' && !modal.classList.contains('hidden')) {
+        closeModal();
+        addNewBook();
+        console.log(myLibrary)
+    }
+ })
+ 
+ 
 
 
 
