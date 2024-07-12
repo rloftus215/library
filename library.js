6cd097019cd233b7977ee79a29ad9c6392c36b0b
rloftus215 <rloftus215@gmail.com>
2024-07-12 11:55:06 +0100

    make object constructor for new books

--- library.js
+++ library.js
@@ -1,5 +1,17 @@
+const library = []
 
- 
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${this.title} by ${this.author}, ${pages} pages, ${read}`
+  }
+}
+
+
+
 
  
 
